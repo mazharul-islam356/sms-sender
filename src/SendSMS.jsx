723e731b392ecce45b7cef074a7f43cc2a428237
rifat3568 <mazharulislam3569@gmail.com>
2024-08-10 19:57:57 +0600

    sms send succesfully from 1 input

--- src/SendSMS.jsx
+++ src/SendSMS.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 const SendSMS = () => {
 
@@ -6,12 +7,28 @@ const SendSMS = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const form = e.target
-        const message = form.message.value
-        const numbers = form.numbers.value
-        console.log(message,numbers);
+        const message = form.message.value;
+        const numbers = form.numbers.value;
+        const formData = {message}
+        console.log(formData);
 
         // const phoneNumbers = numbers.split(',').map(num => num.trim());
+        
+    // axios.post('http://localhost:5002/sendSms', message)
+    // .then((response) => {
+    //   console.log('Data sent successfully:', response.data);
+    // })
+    // .catch((error) => {
+    //   console.error('There was an error sending the data:', error);
+    // });
 
+    axios.post('http://localhost:5002/submit', formData)
+            .then(response => {
+                console.log('Response from server:', response.data);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
        
 
     }
@@ -19,11 +36,13 @@ const SendSMS = () => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-
+<h1 className="text-3xl font-bold mb-4">Sms sender!</h1>
+            <div className="flex flex-col gap-2 justify-center items-center">
             <input name="numbers" type="text" placeholder="Number" className="input input-bordered w-full max-w-xs" />
 
-            <textarea name="message" className="textarea textarea-bordered" placeholder="Message"></textarea>
-            <button className="btn" type="submit">Send Message</button>
+<textarea name="message" className="textarea w-80 textarea-bordered" placeholder="Message"></textarea>
+            </div>
+            <button className="btn btn-info btn-outline mt-2" type="submit">Send Message</button>
         </form>
         </div>
     );
